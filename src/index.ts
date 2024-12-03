6a0f9d6fc6ebd46e743f4c167dae0fb969628ff2
Keyslam <justinvanderleij@gmail.com>
2024-12-03 07:43:45 +0100

    Add simple benchmarking

--- src/index.ts
+++ src/index.ts
@@ -3,20 +3,25 @@ import getInput from './shared/getInput';
 
 import { argv } from 'process';
 
-async function runDay(dayIndex: string) {
+async function runDay(dayIndex: string, benchmark: boolean) {
 	try {
 		const day: Day = (await import(`./days/day${dayIndex}.js`)).default;
 		const input = await getInput(dayIndex);
 
+		if (benchmark) console.time('first_solution');
 		const first_solution = day.solve_first(input);
 		console.log(`First solution: '${first_solution}'`);
+		if (benchmark) console.timeLog('first_solution');
 
+		if (benchmark) console.time('second_solution');
 		const second_solution = day.solve_second(input);
 		console.log(`Second solution: '${second_solution}'`);
+		if (benchmark) console.timeLog('second_solution');
 	} catch (error) {
 		console.error(`Error loading day ${day}:`, error.message);
 	}
 }
 
 const day = argv[2];
-runDay(day);
+const benchmark = argv[3] === '1';
+runDay(day, benchmark);
